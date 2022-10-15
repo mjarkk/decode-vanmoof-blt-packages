aa639c7c11163ef34aca0e9a313c9c442541b303
mjarkk <mkopenga@gmail.com>
2022-10-15 18:09:58 +0200

    Start of a rust rewrite

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+mod parser;
+
+use std::{fs::File, io::Read};
+
+struct Args {
+    file: String,
+    encryption_key: Option<String>,
+    bike_id: Option<String>,
+    hide_challenges: bool,
+    show_only_first_part_of_uuid: bool,
+}
+
+fn main() {
+    let args = Args {
+        file: String::from("bt_snoop.log"),
+        encryption_key: None,
+        bike_id: None,
+        hide_challenges: false,
+        show_only_first_part_of_uuid: false,
+    };
+
+    let mut log_file = File::open(args.file).expect("Error opening logfile");
+
+    let mut contents: Vec<u8> = Vec::new();
+    log_file
+        .read_to_end(&mut contents)
+        .expect("Error reading file");
+
+    if !contents
+        .drain(..8)
+        .eq(vec![0x62, 0x74, 0x73, 0x6e, 0x6f, 0x6f, 0x70, 0x00])
+    {
+        panic!("not a btl snoop file");
+    }
+    let mut content_bytes = parser::Bytes::new(contents);
+
+    let version_number = content_bytes.drain_u32_be();
+    if version_number != 1 {
+        panic!("only blt snoop version 1 is supported, got {version_number}")
+    }
+
+    let data_link_type = content_bytes.drain_u32_be();
+    match data_link_type {
+        1001 => {} // Expected
+        1002 => panic!("Unsupported Datalink Type: HCI UART (H4)"),
+        1003 => panic!("Unsupported Datalink Type: HCI BSCP"),
+        1004 => panic!("Unsupported Datalink Type: HCI Serial (H5)"),
+        1005 => panic!("Unsupported Datalink Type: Unassigned"),
+        data_link_type => {
+            panic!("Unsupported Datalink Type: Reserved / Unassigned ({data_link_type})")
+        }
+    }
+
+    // Firstly lets parse the packages
+    let mut nr = 0usize;
+    let mut packages: Vec<parser::BltPacket> = Vec::new();
+    loop {
+        if content_bytes.len() == 0 {
+            break;
+        }
+
+        nr += 1;
+
+        let original_len = content_bytes.drain_u32_be() as usize;
+        let included_len = content_bytes.drain_u32_be() as usize;
+        content_bytes.drain_u32_be(); // packet_record_len
+        content_bytes.drain_u32_be(); // comulative_drops
+        content_bytes.drain_u32_be(); // timestap_seconds
+        content_bytes.drain_u32_be(); // timestap_microseconds
+
+        let packet_data = content_bytes.drain_bytes(included_len);
+        if packet_data.len() > 0 {
+            let parsed_packet = parser::BltPacket::parse(packet_data, nr);
+            if let Some(packet) = parsed_packet {
+                packages.push(packet);
+            }
+        }
+
+        let padd_size = original_len - included_len;
+        if padd_size > 0 {
+            content_bytes.drain(padd_size);
+        }
+    }
+}
---
+++ src/parser.rs
@@ -0,0 +1,81 @@
+pub struct Bytes(Vec<u8>);
+
+impl Bytes {
+    pub fn new(b: Vec<u8>) -> Self {
+        Self(b)
+    }
+    pub fn drain_u32_be(&mut self) -> u32 {
+        u32::from_be_bytes(self.0.drain(..4).collect::<Vec<u8>>().try_into().unwrap())
+    }
+    pub fn drain_vec(&mut self, range: usize) -> Vec<u8> {
+        self.0.drain(..range).collect()
+    }
+    pub fn drain_bytes(&mut self, range: usize) -> Bytes {
+        Self(self.drain_vec(range))
+    }
+    pub fn drain(&mut self, range: usize) {
+        self.0.drain(..range);
+    }
+    pub fn drain_one(&mut self) -> u8 {
+        *self.0.drain(..1).as_slice().first().unwrap()
+    }
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+pub struct BltPacket {
+    nr: usize,
+    kind: BltPacketKind,
+}
+
+pub enum BltPacketKind {
+    Event(BltEvent),
+}
+
+impl BltPacket {
+    pub fn parse(mut b: Bytes, nr: usize) -> Option<Self> {
+        let kind = match b.drain_one() {
+            0x3e => Some(BltPacketKind::Event(BltEvent::parse(&mut b, nr))),
+            _ => None,
+        };
+
+        if let Some(kind) = kind {
+            Some(Self { nr, kind })
+        } else {
+            None
+        }
+    }
+}
+
+pub enum BltEvent {
+    AdvertizingReport,
+    EnhancedConnectionComplete,
+    UnimportantSubEvent(u8),
+    UnknownSubEvent(u8),
+}
+
+impl BltEvent {
+    fn parse(b: &mut Bytes, nr: usize) -> Self {
+        b.drain_one(); // parameter_total_len
+        let sub_event = b.drain_one();
+        match sub_event {
+            0x02 => {
+                // Sub Event: LE Advertizing report (0x02)
+                Self::AdvertizingReport
+            }
+            0x0a => {
+                // Sub Event: LE Enhanced Connection Complete (0x0a)
+                Self::EnhancedConnectionComplete
+            }
+            0x04 | 0x06 | 0x03 => {
+                // Sub Event: LE Connection Update Complete (0x03)
+                // Sub Event: LE Read Remote Features Complete (0x04)
+                // Sub Event: LE Remote Connection Parameter Request (0x06)
+                // These sub commands do not contain any vauluable information
+                Self::UnimportantSubEvent(sub_event)
+            }
+            _ => Self::UnknownSubEvent(sub_event),
+        }
+    }
+}
